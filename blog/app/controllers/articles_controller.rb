bf43093e7086b6d2f758d98598b2e99b31f10662
shimeikou <shimeikou@tayutau.co.jp>
2018-04-16 00:03:52 +0900

    CRUD完成まで

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,19 +1,47 @@
 class ArticlesController < ApplicationController
     protect_from_forgery except: :create
     def new
-        
+        @article = Article.new
     end
 
     def create
         @article = Article.new(article_params)
 
-        @article.save
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
 
-        redirect_to @article
     end
 
     def show
         @article = Article.find(params[:id])
+    end
+
+    def index
+        @articles = Article.all
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+   
+        if @article.update(article_params)
+            redirect_to @article
+        else
+        render 'edit'
+        end
     end
 
     private
