97846e6880bce2d85ad20ce805d31e67a43dec5d
meikoushi <shimeikou@tayutau.co.jp>
2018-04-18 20:48:04 +0900

    commenter修正

--- blog/db/migrate/20180416032843_create_comments.rb
+++ blog/db/migrate/20180416032843_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
-      t.string :comenter
+      t.string :commenter
       t.text :body
       t.references :article, foreign_key: true
 
