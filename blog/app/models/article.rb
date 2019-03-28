5540cd9f84f0114aeed7897dc57e480214b8340c
meikoushi <shimeikou@tayutau.co.jp>
2019-03-28 11:08:35 +0900

    Ruby合わないわやっぱ

--- blog/app/models/article.rb
+++
@@ -1,7 +0,0 @@
-class Article < ApplicationRecord
-    has_many :comments,dependent: :destroy
-    #一つのArticleには複数のcommentsがつく。　
-    validates :title, presence:true,
-    length: {minimum:5}
-    #データに対してタイトルは空白もしくは5文字以下だとfalseで返す
-end
--- blog/app/models/comment.rb
+++
@@ -1,4 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :article
-  #belongs_to  つまりコメントと記事は一対一が基本
-end
